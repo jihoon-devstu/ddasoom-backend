1d2e8e0159d556f0bcf64ac4baa3368cca1efee0
horangdev1513 <horang1513@naver.com>
2026-07-09 12:04:36 +0900

    ✅ test: 댓글 수 테스트코드 작성

--- src/test/java/com/paw/ddasoom/board/domain/PostTest.java
+++ src/test/java/com/paw/ddasoom/board/domain/PostTest.java
@@ -45,4 +45,26 @@ public class PostTest {
         assertThat(post.getViewCount()).isEqualTo(3);
     }
 
+    // 댓글 수 테스트
+    @Test
+    @DisplayName("댓글 수가 정상적으로 오르고 내린다")
+    void commentCountTest() {
+
+        // given
+        Post post = Post.builder()
+                .title("테스트 제목")
+                .content("테스트 내용")
+                .build();
+
+        // when
+        post.increaseCommentCount();
+        post.increaseCommentCount();
+        post.decreaseCommentCount();
+
+        // then
+        assertThat(post.getCommentCount()).isEqualTo(1);
+    }
+
+
+
 }

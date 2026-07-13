bf671feb1bfa842114112c6867e1cc9deae2a8a4
horangdev1513 <horang1513@naver.com>
2026-07-13 16:13:56 +0900

    ✨ feat: PostCreateRequest, PostUpdateRequest 하이브리드 업로드용, 대표 이미지 명시 지정용 필드 추가 (imagesIds, thumbnailImageId)

--- src/main/java/com/paw/ddasoom/board/dto/request/PostUpdateRequest.java
+++ src/main/java/com/paw/ddasoom/board/dto/request/PostUpdateRequest.java
@@ -4,6 +4,8 @@ import com.paw.ddasoom.board.domain.BoardType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 public class PostUpdateRequest {
@@ -12,4 +14,6 @@ public class PostUpdateRequest {
     private String category;
     private String title;
     private String content;
+    private List<Long> imageIds;        // 하이브리드 업로드 확정 연결용
+    private Long thumbnailImageId;      // 대표 이미지 명시 지정 (nullable)
 }
